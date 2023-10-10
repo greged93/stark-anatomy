fae18f72daaabc34f8b93fda670d664888162704
Gregory Edison <gregory.edison1993@gmail.com>
2023-10-10 18:39:51 +0200

    update to I320

--- src/field/types/base.rs
+++ src/field/types/base.rs
@@ -2,16 +2,16 @@ use ruint::algorithms::div::div;
 use std::ops;
 
 #[derive(Clone, Copy, Debug)]
-pub struct I256 {
-    value: [u64; 4],
+pub struct I320 {
+    value: [u64; 5],
 }
 
-impl I256 {
-    pub const ZERO: I256 = I256 {
-        value: [0, 0, 0, 0],
+impl I320 {
+    pub const ZERO: I320 = I320 {
+        value: [0, 0, 0, 0, 0],
     };
-    pub const ONE: I256 = I256 {
-        value: [1, 0, 0, 0],
+    pub const ONE: I320 = I320 {
+        value: [1, 0, 0, 0, 0],
     };
 
     pub fn pow(self, exponent: Self) -> Self {
@@ -37,15 +37,15 @@ impl I256 {
     }
 
     fn sign(&self) -> bool {
-        (self.value[3] & 0x8000000000000000) != 0
+        (self.value[4] & 0x8000000000000000) != 0
     }
 }
 
-impl ops::Add<I256> for I256 {
+impl ops::Add<I320> for I320 {
     type Output = Self;
 
     fn add(self, rhs: Self) -> Self::Output {
-        let mut result = [0u64; 4];
+        let mut result = [0u64; 5];
         let mut carry = 0u64;
         for (i, v) in self.value.into_iter().enumerate() {
             let (sum, carry1) = v.overflowing_add(rhs.value[i]);
@@ -57,7 +57,7 @@ impl ops::Add<I256> for I256 {
     }
 }
 
-impl ops::Sub<I256> for I256 {
+impl ops::Sub<I320> for I320 {
     type Output = Self;
 
     fn sub(self, rhs: Self) -> Self::Output {
@@ -65,24 +65,24 @@ impl ops::Sub<I256> for I256 {
     }
 }
 
-impl ops::Neg for I256 {
+impl ops::Neg for I320 {
     type Output = Self;
 
     fn neg(self) -> Self::Output {
         let mut signed = self;
         let mut carry = true;
-        for i in 0..4 {
+        for i in 0..5 {
             (signed.value[i], carry) = (!signed.value[i]).overflowing_add(carry as u64);
         }
         signed
     }
 }
 
-impl ops::Mul<I256> for I256 {
+impl ops::Mul<I320> for I320 {
     type Output = Self;
 
     fn mul(self, rhs: Self) -> Self::Output {
-        let mut result = [0u64; 9];
+        let mut result = [0u64; 10];
         let mut carry = false;
         for (i, v) in self.value.iter().enumerate() {
             for (j, w) in rhs.value.iter().enumerate() {
@@ -95,12 +95,12 @@ impl ops::Mul<I256> for I256 {
             }
         }
         Self {
-            value: [result[0], result[1], result[2], result[3]],
+            value: [result[0], result[1], result[2], result[3], result[4]],
         }
     }
 }
 
-impl ops::Div<I256> for I256 {
+impl ops::Div<I320> for I320 {
     type Output = Self;
 
     fn div(self, rhs: Self) -> Self::Output {
@@ -114,7 +114,7 @@ impl ops::Div<I256> for I256 {
     }
 }
 
-impl ops::Rem<I256> for I256 {
+impl ops::Rem<I320> for I320 {
     type Output = Self;
 
     fn rem(self, rhs: Self) -> Self::Output {
@@ -128,13 +128,13 @@ impl ops::Rem<I256> for I256 {
     }
 }
 
-impl PartialEq for I256 {
+impl PartialEq for I320 {
     fn eq(&self, other: &Self) -> bool {
         self.value == other.value
     }
 }
 
-impl std::cmp::PartialOrd for I256 {
+impl std::cmp::PartialOrd for I320 {
     fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
         let l_sign = self.sign();
         let r_sign = other.sign();
@@ -153,29 +153,29 @@ impl std::cmp::PartialOrd for I256 {
     }
 }
 
-impl From<u64> for I256 {
+impl From<u64> for I320 {
     fn from(num: u64) -> Self {
         Self {
-            value: [num, 0, 0, 0],
+            value: [num, 0, 0, 0, 0],
         }
     }
 }
 
-impl From<u128> for I256 {
+impl From<u128> for I320 {
     fn from(num: u128) -> Self {
         Self {
-            value: [num as u64, (num >> 64) as u64, 0, 0],
+            value: [num as u64, (num >> 64) as u64, 0, 0, 0],
         }
     }
 }
 
-impl From<I256> for u128 {
-    fn from(num: I256) -> Self {
+impl From<I320> for u128 {
+    fn from(num: I320) -> Self {
         (num.value[1] as u128) << 64 | num.value[0] as u128
     }
 }
 
-impl From<i64> for I256 {
+impl From<i64> for I320 {
     fn from(num: i64) -> Self {
         if num < 0 {
             -Self::from(-num as u64)
@@ -185,7 +185,7 @@ impl From<i64> for I256 {
     }
 }
 
-impl From<i128> for I256 {
+impl From<i128> for I320 {
     fn from(num: i128) -> Self {
         if num < 0 {
             -Self::from(-num as u128)
@@ -202,16 +202,16 @@ mod tests {
     #[test]
     fn test_pow() {
         // Given
-        let a = I256 {
-            value: [2, 0, 0, 0],
+        let a = I320 {
+            value: [2, 0, 0, 0, 0],
         };
 
         // When
-        let result = a.pow(I256::from(100u64));
+        let result = a.pow(I320::from(100u64));
 
         // Then
-        let expected = I256 {
-            value: [0, 68719476736, 0, 0],
+        let expected = I320 {
+            value: [0, 68719476736, 0, 0, 0],
         };
         assert_eq!(expected.value, result.value);
     }
@@ -219,16 +219,16 @@ mod tests {
     #[test]
     fn test_neg() {
         // Given
-        let a = I256 {
-            value: [u64::MAX, u64::MAX, 0, 0],
+        let a = I320 {
+            value: [u64::MAX, u64::MAX, 0, 0, 0],
         };
 
         // When
         let result = -a;
 
         // Then
-        let expected = I256 {
-            value: [1, 0, u64::MAX, u64::MAX],
+        let expected = I320 {
+            value: [1, 0, u64::MAX, u64::MAX, u64::MAX],
         };
         assert_eq!(expected.value, result.value);
     }
@@ -236,19 +236,19 @@ mod tests {
     #[test]
     fn test_add() {
         // Given
-        let a = I256 {
-            value: [u64::MAX, u64::MAX, 0, 0],
+        let a = I320 {
+            value: [u64::MAX, u64::MAX, 0, 0, 0],
         };
-        let b = I256 {
-            value: [u64::MAX, u64::MAX, 0, 0],
+        let b = I320 {
+            value: [u64::MAX, u64::MAX, 0, 0, 0],
         };
 
         // When
         let result = a + b;
 
         // Then
-        let expected = I256 {
-            value: [u64::MAX - 1, u64::MAX, 1, 0],
+        let expected = I320 {
+            value: [u64::MAX - 1, u64::MAX, 1, 0, 0],
         };
         assert_eq!(expected.value, result.value);
     }
@@ -256,19 +256,19 @@ mod tests {
     #[test]
     fn test_add_neg() {
         // Given
-        let a = I256 {
-            value: [u64::MAX, u64::MAX, 0, 0],
+        let a = I320 {
+            value: [u64::MAX, u64::MAX, 0, 0, 0],
         };
-        let b = I256 {
-            value: [u64::MAX, u64::MAX, 0, 0],
+        let b = I320 {
+            value: [u64::MAX, u64::MAX, 0, 0, 0],
         };
 
         // When
         let result = a - b;
 
         // Then
-        let expected = I256 {
-            value: [0, 0, 0, 0],
+        let expected = I320 {
+            value: [0, 0, 0, 0, 0],
         };
         assert_eq!(expected.value, result.value);
     }
@@ -276,31 +276,69 @@ mod tests {
     #[test]
     fn test_mul() {
         // Given
-        let a = I256 {
-            value: [1, 2, 3, 4],
+        let a = I320 {
+            value: [1, 2, 3, 4, 0],
         };
-        let b = I256 {
-            value: [5, 6, 7, 8],
+        let b = I320 {
+            value: [5, 6, 7, 8, 0],
         };
 
         // When
         let result = a * b;
 
         // Then
-        let expected = I256 {
-            value: [5, 16, 34, 60],
+        let expected = I320 {
+            value: [5, 16, 34, 60, 61], // TODO check that this is correct
         };
         assert_eq!(expected.value, result.value);
     }
 
     #[test]
+    fn test_mul_near_boundary() {
+        // `PRIME` is the expression `1 + 407 * 2u128.pow(119)` evaluated
+        // see: https://github.com/aszepieniec/stark-anatomy/blob/76c375505a28e7f02f8803f77f8d7620d834071d/docs/basic-tools.md?plain=1#L113-L119
+        //
+        // This test is essential for ensuring the robustness and correctness of our multiplication
+        // implementation, especially when values approach the maximum representable in the I320 type.
+        //
+        // In many cryptographic contexts, especially those that use modular arithmetic (like STARKs),
+        // operations involving numbers near the modulus (in this case, PRIME) are common. This test
+        // simulates a "worst-case scenario" where two values just shy of the modulus are multiplied
+        // together. The results of such multiplications can potentially overflow the I320 type.
+        //
+        // Two things are being checked here:
+        // 1. That the raw multiplication (without considering any modulus) is correct. This checks
+        //    if the system handles potential overflows correctly. The result is compared against a
+        //    saturating multiplication to ensure no unintended wrap-around occurs.
+        // 2. That the multiplication result modulo PRIME is as expected. In modular arithmetic,
+        //    multiplying two numbers both equal to (PRIME - 1) should yield a result of 1 when taken modulo PRIME.
+        //
+        // Ensuring correctness for this boundary condition is crucial for the overall reliability of
+        // any system built on top of this arithmetic foundation.
+
+        // Given the boundary condition where PRIME is close to the maximum value for I320. [-2^255 + 1; 2^255 - 1]
+        let prime = 270497897142230380135924736767050121217_u128;
+        let prime_minus_one = prime - 1_u128;
+        let near_boundary = I320::from(prime_minus_one);
+
+        // When
+        let result = near_boundary * near_boundary;
+
+        // Then
+        // Expected result computation depends on the desired behavior.
+        let mod_prime = result % I320::from(prime);
+
+        assert_eq!(I320::ONE, mod_prime);
+    }
+
+    #[test]
     fn test_compare_pos_pos() {
         // Given
-        let a = I256 {
-            value: [1, 2, 3, 4],
+        let a = I320 {
+            value: [1, 2, 3, 4, 0],
         };
-        let b = I256 {
-            value: [5, 6, 7, 8],
+        let b = I320 {
+            value: [5, 6, 7, 8, 0],
         };
 
         // When
@@ -317,11 +355,11 @@ mod tests {
     #[test]
     fn test_compare_pos_neg() {
         // Given
-        let a = I256 {
-            value: [1, 2, 3, 4],
+        let a = I320 {
+            value: [1, 2, 3, 4, 0],
         };
-        let b = -I256 {
-            value: [5, 6, 7, 8],
+        let b = -I320 {
+            value: [5, 6, 7, 8, 0],
         };
 
         // When
@@ -338,11 +376,11 @@ mod tests {
     #[test]
     fn test_compare_neg_pos() {
         // Given
-        let a = -I256 {
-            value: [1, 2, 3, 4],
+        let a = -I320 {
+            value: [1, 2, 3, 4, 0],
         };
-        let b = I256 {
-            value: [5, 6, 7, 8],
+        let b = I320 {
+            value: [5, 6, 7, 8, 0],
         };
 
         // When
@@ -359,11 +397,11 @@ mod tests {
     #[test]
     fn test_compare_neg_neg() {
         // Given
-        let a = -I256 {
-            value: [1, 2, 3, 4],
+        let a = -I320 {
+            value: [1, 2, 3, 4, 0],
         };
-        let b = -I256 {
-            value: [5, 6, 7, 8],
+        let b = -I320 {
+            value: [5, 6, 7, 8, 0],
         };
 
         // When
@@ -380,19 +418,19 @@ mod tests {
     #[test]
     fn test_division() {
         // Given
-        let a = I256 {
-            value: [5, 6, 7, 8],
+        let a = I320 {
+            value: [5, 6, 7, 8, 0],
         };
-        let b = I256 {
-            value: [1, 2, 3, 4],
+        let b = I320 {
+            value: [1, 2, 3, 4, 0],
         };
 
         // When
         let result = a / b;
 
         // Then
-        let expected = I256 {
-            value: [2, 0, 0, 0],
+        let expected = I320 {
+            value: [2, 0, 0, 0, 0],
         };
         assert_eq!(expected.value, result.value);
     }
@@ -400,19 +438,19 @@ mod tests {
     #[test]
     fn test_rem_simple() {
         // Given
-        let a = I256 {
-            value: [5, 6, 7, 8],
+        let a = I320 {
+            value: [5, 6, 7, 8, 0],
         };
-        let b = I256 {
-            value: [1, 2, 3, 4],
+        let b = I320 {
+            value: [1, 2, 3, 4, 0],
         };
 
         // When
         let result = a % b;
 
         // Then
-        let expected = I256 {
-            value: [3, 2, 1, 0],
+        let expected = I320 {
+            value: [3, 2, 1, 0, 0],
         };
         let div = a / b;
         assert_eq!(expected.value, result.value);
@@ -422,17 +460,17 @@ mod tests {
     #[test]
     fn test_rem_complex() {
         // Given
-        let a = I256 {
-            value: [0, 0, 4294967296, 0],
+        let a = I320 {
+            value: [0, 0, 4294967296, 0, 0],
         };
-        let b = I256::from(1 + 407 * 2u128.pow(119));
+        let b = I320::from(1 + 407 * 2u128.pow(119));
 
         // When
         let result = a % b;
 
         // Then
-        let expected = I256 {
-            value: [18446744068306546075, 13150510911921848319, 0, 0],
+        let expected = I320 {
+            value: [18446744068306546075, 13150510911921848319, 0, 0, 0],
         };
         assert_eq!(expected.value, result.value);
     }
@@ -440,17 +478,17 @@ mod tests {
     #[test]
     fn test_rem_complex_negative_modulus() {
         // Given
-        let a = I256 {
-            value: [0, 0, 4294967296, 0],
+        let a = I320 {
+            value: [0, 0, 4294967296, 0, 0],
         };
-        let b = I256::from(1 + 407 * 2u128.pow(119));
+        let b = I320::from(1 + 407 * 2u128.pow(119));
 
         // When
         let result = a % (-b);
 
         // Then
-        let expected = I256 {
-            value: [18446744068306546075, 13150510911921848319, 0, 0],
+        let expected = I320 {
+            value: [18446744068306546075, 13150510911921848319, 0, 0, 0],
         };
         assert_eq!(expected.value, result.value);
     }
@@ -458,17 +496,17 @@ mod tests {
     #[test]
     fn test_rem_complex_negative_dividend() {
         // Given
-        let a = I256 {
-            value: [0, 0, 4294967296, 0],
+        let a = I320 {
+            value: [0, 0, 4294967296, 0, 0],
         };
-        let b = I256::from(1 + 407 * 2u128.pow(119));
+        let b = I320::from(1 + 407 * 2u128.pow(119));
 
         // When
         let result = (-a) % b;
 
         // Then
-        let expected = -I256 {
-            value: [18446744068306546075, 13150510911921848319, 0, 0],
+        let expected = -I320 {
+            value: [18446744068306546075, 13150510911921848319, 0, 0, 0],
         };
         assert_eq!(expected.value, result.value);
     }
@@ -479,11 +517,11 @@ mod tests {
         let a = 5u64;
 
         // When
-        let result = I256::from(a);
+        let result = I320::from(a);
 
         // Then
-        let expected = I256 {
-            value: [5, 0, 0, 0],
+        let expected = I320 {
+            value: [5, 0, 0, 0, 0],
         };
         assert_eq!(expected.value, result.value);
     }
@@ -494,11 +532,11 @@ mod tests {
         let a = u64::MAX as u128 + 2;
 
         // When
-        let result = I256::from(a);
+        let result = I320::from(a);
 
         // Then
-        let expected = I256 {
-            value: [1, 1, 0, 0],
+        let expected = I320 {
+            value: [1, 1, 0, 0, 0],
         };
         assert_eq!(expected.value, result.value);
     }
@@ -506,8 +544,8 @@ mod tests {
     #[test]
     fn test_from_i256_to_u128() {
         // Given
-        let a = I256 {
-            value: [u64::MAX, u64::MAX, 0, 0],
+        let a = I320 {
+            value: [u64::MAX, u64::MAX, 0, 0, 0],
         };
 
         // When
@@ -524,11 +562,11 @@ mod tests {
         let a = -5i64;
 
         // When
-        let result = I256::from(a);
+        let result = I320::from(a);
 
         // Then
-        let expected = -I256 {
-            value: [5, 0, 0, 0],
+        let expected = -I320 {
+            value: [5, 0, 0, 0, 0],
         };
         assert_eq!(expected.value, result.value);
     }
@@ -539,11 +577,11 @@ mod tests {
         let a = -(u64::MAX as i128 + 2);
 
         // When
-        let result = I256::from(a);
+        let result = I320::from(a);
 
         // Then
-        let expected = -I256 {
-            value: [1, 1, 0, 0],
+        let expected = -I320 {
+            value: [1, 1, 0, 0, 0],
         };
         assert_eq!(expected.value, result.value);
     }
--- src/field/utils.rs
+++ src/field/utils.rs
@@ -1,9 +1,9 @@
-use crate::field::types::base::I256;
+use crate::field::types::base::I320;
 
-pub(crate) fn extended_euclidean(a: I256, b: I256) -> (I256, I256, I256) {
+pub(crate) fn extended_euclidean(a: I320, b: I320) -> (I320, I320, I320) {
     let (mut old_r, mut r) = (a, b);
-    let (mut old_s, mut s) = (I256::ONE, I256::ZERO);
-    let (mut old_t, mut t) = (I256::ZERO, I256::ONE);
+    let (mut old_s, mut s) = (I320::ONE, I320::ZERO);
+    let (mut old_t, mut t) = (I320::ZERO, I320::ONE);
 
     loop {
         let q = old_r / r;
@@ -11,7 +11,7 @@ pub(crate) fn extended_euclidean(a: I256, b: I256) -> (I256, I256, I256) {
         (old_r, r) = (r, old_r - q * r);
         (old_s, s) = (s, old_s - q * s);
         (old_t, t) = (t, old_t - q * t);
-        if r == I256::ZERO {
+        if r == I320::ZERO {
             return (old_r, old_s, old_t);
         }
     }
@@ -24,30 +24,30 @@ mod tests {
     #[test]
     fn test_basic_extended_eucledian() {
         // Given
-        let a = I256::from(240u64);
-        let b = I256::from(46u64);
+        let a = I320::from(240u64);
+        let b = I320::from(46u64);
 
         // When
         let (g, s, t) = extended_euclidean(a, b);
 
         // Then
-        assert_eq!(g, I256::from(2u64));
-        assert_eq!(s, I256::from(-9i64));
-        assert_eq!(t, I256::from(47u64));
+        assert_eq!(g, I320::from(2u64));
+        assert_eq!(s, I320::from(-9i64));
+        assert_eq!(t, I320::from(47u64));
     }
 
     #[test]
     fn test_complex_extended_eucledian() {
         // Given
-        let a = I256::from(6543211245u64);
-        let b = I256::from(123456785u64);
+        let a = I320::from(6543211245u64);
+        let b = I320::from(123456785u64);
 
         // When
         let (g, s, t) = extended_euclidean(a, b);
 
         // Then
-        assert_eq!(g, I256::from(5u64));
-        assert_eq!(s, I256::from(6850346u64));
-        assert_eq!(t, I256::from(-363068429i64));
+        assert_eq!(g, I320::from(5u64));
+        assert_eq!(s, I320::from(6850346u64));
+        assert_eq!(t, I320::from(-363068429i64));
     }
 }
